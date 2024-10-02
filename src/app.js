0d418959d68090fef930761f627b8dc13a971f5d
irivinti-gayathri <gayathriirivinti@gmail.com>
2024-10-02 18:16:21 +0530

    playing with symbols in route

--- src/app.js
+++ src/app.js
@@ -1,30 +1,27 @@
 const express = require("express");
 const app = express();
-// app.use("/", (req, res) => {
-//   res.send("Hello!");
-// });
-// app.use("/hello", (req, res) => {
-//   res.send("Hello from the hello!");
-// });
-// app.use("/test", (req, res) => {
-//   res.send("Hello from the test server!");
-// });
-app.get("/user", (req, res) => {
+app.get("/a*d", (req, res) => {
   res.send({firstname:"sai",lastname:"bhanu"});
 });
-app.post("/user", (req, res) => {
-  res.send("data added to db successfully");
-});
-app.patch("/user",(req,res)=>
-{
-  res.send("data updated sucessfully");
-})
-app.delete("/user", (req, res) => {
-  res.send("data deleted from db successfully");
-});
-
-
-
+// app.get("/a(bc)?d", (req, res) => {
+//   res.send({firstname:"sai",lastname:"bhanu"});
+// });
+// app.get("/a(bc)+d", (req, res) => {
+//   res.send({firstname:"sai",lastname:"bhanu"});
+// });
+// app.get("/user", (req, res) => {
+//  res.send({firstname:"sai",lastname:"bhanu"});
+// });
+// app.post("/user", (req, res) => {
+//   res.send("data added to db successfully");
+// });
+// app.patch("/user",(req,res)=>
+// {
+//   res.send("data updated sucessfully");
+// })
+// app.delete("/user", (req, res) => {
+//   res.send("data deleted from db successfully");
+// });
 app.listen(3000, () => {
   console.log("Server is listening to port 3000....");
 });
