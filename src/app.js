c1ddcbaa6e79a5856ac2c0ecb617b5a78f5ff9d1
irivinti-gayathri <gayathriirivinti@gmail.com>
2024-10-02 18:31:17 +0530

    dynamic data to routes

--- src/app.js
+++ src/app.js
@@ -1,32 +1,8 @@
 const express = require("express");
 const app = express();
-// app.get("/a*d", (req, res) => {
-//   res.send({firstname:"sai",lastname:"bhanu"});
-// });
-// // app.get("/a(bc)?d", (req, res) => {
-//   res.send({firstname:"sai",lastname:"bhanu"});
-// });
-// app.get("/a(bc)+d", (req, res) => {
-//   res.send({firstname:"sai",lastname:"bhanu"});
-// });
-// app.get("/user", (req, res) => {
-//  res.send({firstname:"sai",lastname:"bhanu"});
-// });
-// app.post("/user", (req, res) => {
-//   res.send("data added to db successfully");
-// });
-// app.patch("/user",(req,res)=>
-// {
-//   res.send("data updated sucessfully");
-// })
-// app.delete("/user", (req, res) => {
-//   res.send("data deleted from db successfully");
-// });
 
-app.get(/a/, (req, res) => {
-  res.send({firstname:"sai",lastname:"bhanu"});
-});
-app.get(/.*abc$/, (req, res) => {
+app.get("/user/:userid", (req, res) => {
+  console.log(req.params);
   res.send({firstname:"sai",lastname:"bhanu"});
 });
 
