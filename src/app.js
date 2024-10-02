ca4e4e54ed42c2271fad7b779866ab33d2755627
irivinti-gayathri <gayathriirivinti@gmail.com>
2024-10-02 19:44:29 +0530

    Handling Multiple Rote Handlers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
 
-app.get("/user/:userid", (req, res) => {
-  console.log(req.params);
-  res.send({firstname:"sai",lastname:"bhanu"});
-});
+app.get("/user", 
+  (req, res,next) => {
+  console.log("Handling route handler 1");
+ // res.send("1st Route Handler..");
+  next();
+},
+[(req, res,next) => {
+  console.log("Handling route handler 2");
+  next();
+},
+(req, res,next) => {
+  console.log("Handling route handler 3");
+  next();
+}],
+(req, res) => {
+  console.log("Handling route handler 4");
+  res.send("4th Route Handler..");
+},
+);
 
 app.listen(3000, () => {
   console.log("Server is listening to port 3000....");
