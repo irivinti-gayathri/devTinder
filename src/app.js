71534a58116d27c48179288df0ed84bba8221a02
irivinti-gayathri <gayathriirivinti@gmail.com>
2024-10-02 18:20:36 +0530

    using regex in routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-app.get("/a*d", (req, res) => {
-  res.send({firstname:"sai",lastname:"bhanu"});
-});
-// app.get("/a(bc)?d", (req, res) => {
+// app.get("/a*d", (req, res) => {
+//   res.send({firstname:"sai",lastname:"bhanu"});
+// });
+// // app.get("/a(bc)?d", (req, res) => {
 //   res.send({firstname:"sai",lastname:"bhanu"});
 // });
 // app.get("/a(bc)+d", (req, res) => {
@@ -22,6 +22,14 @@ app.get("/a*d", (req, res) => {
 // app.delete("/user", (req, res) => {
 //   res.send("data deleted from db successfully");
 // });
+
+app.get(/a/, (req, res) => {
+  res.send({firstname:"sai",lastname:"bhanu"});
+});
+app.get(/.*abc$/, (req, res) => {
+  res.send({firstname:"sai",lastname:"bhanu"});
+});
+
 app.listen(3000, () => {
   console.log("Server is listening to port 3000....");
 });
