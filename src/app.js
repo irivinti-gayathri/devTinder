a96054e00307362545d530b73baed4d9345f00b9
irivinti-gayathri <gayathriirivinti@gmail.com>
2024-10-03 19:57:54 +0530

    Delete Api using dynamic data

--- src/app.js
+++ src/app.js
@@ -3,51 +3,69 @@ const { connectDB } = require("./config/database");
 const { User } = require("./models/user");
 const app = express();
 app.use(express.json());
-app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    res.send("User added sucessfully");
-  } catch (err) {
-    res.status(400).send("Cannot add User" + err.message);
-  }
-});
-app.get("/userbyemail", async (req, res) => {
-  const email = req.body.email;
-  try {
-    const user = await User.findOne({ email});
-    if (!user) {
+// app.post("/signup", async (req, res) => {
+//   const user = new User(req.body);
+//   try {
+//     await user.save();
+//     res.send("User added sucessfully");
+//   } catch (err) {
+//     res.status(400).send("Cannot add User" + err.message);
+//   }
+// });
+// app.get("/userbyemail", async (req, res) => {
+//   const email = req.body.email;
+//   try {
+//     const user = await User.findOne({ email});
+//     if (!user) {
+//       res.status(404).send("User not found");
+//     } else {
+//       res.send(user);
+//     }
+//   } catch (err) {
+//     res.send("something went wrong ");
+//   }
+// });
+// app.get("/userbyid", async (req, res) => {
+//   const _id = req.body._id;
+//   try {
+//     const user = await User.findById({_id});
+//     if (!user) {
+//       res.status(404).send("User not found");
+//     } else {
+//       res.send(user);
+//     }
+//   } catch (err) {
+//     res.send("something went wrong ");
+//   }
+// });
+// app.get("/feed", async (req, res) => {
+//   try {
+//     const users = await User.find({});
+//     if (users.length===0) {
+//       res.status(404).send("User not found");
+//     } else {
+//       res.send(users);
+//     }
+//   } catch (err) {
+//     res.send("something went wrong ");
+//   }
+// });
+app.delete("/userbyId",async(req,res)=>
+{
+  const _id=req.body._id;
+  try{
+    const deleteduser=await User.findByIdAndDelete(_id);
+    if(!deleteduser)
+    {
       res.status(404).send("User not found");
-    } else {
-      res.send(user);
     }
-  } catch (err) {
-    res.send("something went wrong ");
+    else{
+      res.send("Deleted user sucessfully");
+    }
   }
-});
-app.get("/userbyid", async (req, res) => {
-  const _id = req.body._id;
-  try {
-    const user = await User.findById({_id});
-    if (!user) {
-      res.status(404).send("User not found");
-    } else {
-      res.send(user);
-    }
-  } catch (err) {
-    res.send("something went wrong ");
-  }
-});
-app.get("/feed", async (req, res) => {
-  try {
-    const users = await User.find({});
-    if (users.length===0) {
-      res.status(404).send("User not found");
-    } else {
-      res.send(users);
-    }
-  } catch (err) {
-    res.send("something went wrong ");
+  catch(err)
+  {
+    res.send("Something went wrong");
   }
 });
 connectDB()
